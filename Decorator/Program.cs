a9a2342b31cb089ff2f35a39e25d6d29c354eae7
bryanlayderos <70880321+bryanlayderos@users.noreply.github.com>
2020-09-08 14:47:56 +0800

    Add files via upload

---
+++ Decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Decorator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Console.Write("Enter a string: ");
+            input = Console.ReadLine();
+
+            LoggingBehavior stringBehavior = new LoggingBehavior(new AppendingBehavior(new DefaultStringBehavior()));
+
+            stringBehavior.Log(stringBehavior.Reverse(input));
+
+            Console.ReadLine();
+        }
+    }
+}
